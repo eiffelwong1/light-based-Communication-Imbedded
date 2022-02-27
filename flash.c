977b874574c061ba220141e5bc43d2281994ba17
Tianyue-Zhao <tiz001@ucsd.edu>
2022-02-27 22:46:42 +0000

    Flash test with add operations

--- flash.c
+++ flash.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include "params.h"
 #include <stdlib.h>
+#include <time.h>
 
 
 
@@ -18,17 +19,36 @@ int main()
 		printf("Flash: wiringPi setup failed\n");
 	}
 	pinMode(SENDPIN, OUTPUT);
-	int freq, micro;
-	printf("Enter frequency: ");
-	scanf("%d", &freq);
-	printf("%d\n", freq);
-	micro = 1000000 / freq / 2;
-	printf("%d\n", micro);
+
+	struct timespec sleep_time;
+	sleep_time.tv_sec = 0;
+	sleep_time.tv_nsec = 10;
+	struct timespec remaining_time;
+
+	register unsigned int i, k;
+
+	// int freq, micro;
+	// printf("Enter frequency: ");
+	// scanf("%d", &freq);
+	// printf("%d\n", freq);
+	// micro = 1000000 / freq / 2;
+	// printf("%d\n", micro);
 	while(0x2e)
 	{
 		digitalWrite(SENDPIN, HIGH);
-		usleep(micro);
+		// usleep(micro);
+		// nanosleep(&sleep_time, &remaining_time);
+		for(i=0;i<10000;i++)
+		{
+			k++;
+		}
 		digitalWrite(SENDPIN, LOW);
-		usleep(micro);
+		// usleep(micro);
+		// nanosleep(&sleep_time, &remaining_time);
+		for(i=0;i<10000;i++)
+		{
+			k++;
+		}
+		digitalWrite(SENDPIN, LOW);
 	}
 }
