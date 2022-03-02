d079f813d5805745e4ba64e286c971ff3867a003
Tianyue-Zhao <tiz001@ucsd.edu>
2022-03-02 04:13:05 +0000

    Comments 1

--- flash.c
+++ flash.c
@@ -13,17 +13,22 @@ void signal_handler(int signum) {
 
 int main()
 {
+	// Boilerplate
 	if(!wiringPiSetup() == -1)
 	{
 		printf("Flash: wiringPi setup failed\n");
 	}
 	pinMode(SENDPIN, OUTPUT);
 	int freq, micro;
+
+	// Take the frequency from stdin
 	printf("Enter frequency: ");
 	scanf("%d", &freq);
 	printf("%d\n", freq);
+	// Translate into microseconds to sleep
 	micro = 1000000 / freq / 2;
 	printf("%d\n", micro);
+	// Loop until SIGINT
 	while(0x2e)
 	{
 		digitalWrite(SENDPIN, HIGH);
